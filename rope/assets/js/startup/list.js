5af45538b5687ede1f7e5e28d2e493899b3e5493
bfad <sluggy.fan@gmail.com>
2012-12-13 11:54:33 -0500

    Adding styling and better folder show/hide functionality to file browsing
    Folders now collapse if you click on them again instead of duplicating their
    contents underneath themselves again.
    
    Clicking on a file now traps that click so that its parent folder doesn't
    collapse.
    
    Also added CSS3 spinner / throbber to indicate AJAX requests

--- rope/assets/js/startup/list.js
+++ rope/assets/js/startup/list.js
@@ -1,14 +1,24 @@
 $(function(){
-    $("#file_browser").on("click", "li.is_dir", function(e){ loadListingFor($(this)) });
+    $("#file_browser")
+        .on("click", "li.is_dir" , function(e){ loadListingFor($(this), e) })
+        .on("click", "li.is_file", function(e){     reloadFile($(this), e) });
     loadListingFor($("#file_browser"));
 });
 
-function loadListingFor(elm) {
+function loadListingFor(elm, e) {
+    if(e)
+        e.stopPropagation();
+
     if(! elm.data('path'))
         return;
 
-    // Spinner for loading would be nice
+    if(elm.hasClass("is_listing")) {
+        elm.removeClass('is_listing').children('ul').remove();
+        return;
+    }
 
+    // Spinner for loading
+    elm.addClass("is_listing").append('<div id="circleLoading"><div id="circleLoading_1" class="circleLoading"></div><div id="circleLoading_2" class="circleLoading"></div><div id="circleLoading_3" class="circleLoading"></div><br style="clear: left;" /></div>');
     $.get('list.xhr', {path: elm.data('path')}, function(data){
         var html = '<ul>';
         for(i in data.files) {
@@ -18,14 +28,17 @@ function loadListingFor(elm) {
             html += '<li class="is_dir" data-path="' + elm.data('path') + '/' + data.dirs[i] + '">' + data.dirs[i] + '</li>';
         }
         html += '</ul>';
+        elm.children("#circleLoading").remove();
         elm.append(html);
-        restripeFileBrowser();
     }, 'json');
 }
 
-function restripeFileBrowser() {
-    $("#file_browser li").removeClass('alt_color0').removeClass('alt_color1');
+function reloadFile(elm, e) {
+    if(e)
+        e.stopPropagation();
+/*
+    $.get('reload.xhr', {path: elm.data('path')}, function(data) {
 
-    $("#file_browser li:even").addClass('alt_color0');
-    $("#file_browser li:odd") .addClass('alt_color1');
+    }, 'json')
+*/
 }
--- rope/startup/list[html].lasso
+++ rope/startup/list[html].lasso
@@ -5,6 +5,15 @@
     <link rel="stylesheet" type="text/css" charset="utf-8" href="[lassoapp_link('/assets/css/styles.css')]" />
     <script type="text/javascript" src="[lassoapp_link('/assets/js/jquery.js')]"></script>
     <script type="text/javascript" src="[lassoapp_link('/assets/js/startup/list.js')]"></script>
+
+    <style type="text/css">
+        #file_browser li.is_dir {
+            background-image: url("[lassoapp_link('/assets/img/icon_folder.svg')]");
+        }
+        #file_browser li.is_file {
+            background-image: url("[lassoapp_link('/assets/img/icon_lasso_file.svg')]");
+        }
+    </style>
 </head>
 <body class="fixed">
     <h1>LassoStartup Files</h1>
